a00495b568d4832e6ce397ecda8a6f1846e02915
Micha Reiser <micha@famreiser.ch>
2017-04-07 07:53:32 +0200

    Make files (hopefully) Windows compliant

--- packages/compiler/__tests__/code-generation/cases/binary-expression/|.ts
+++
@@ -1,9 +0,0 @@
-async function bar() {
-    "use speedyjs";
-
-    let six = 2 | 4;
-    six |= 8;
-
-    let intValue = 3.56 | 0;
-    let intToInt = 3 | 0;
-}
--- packages/compiler/__tests__/code-generation/cases/binary-expression/||.ts
+++
@@ -1,9 +0,0 @@
-async function barBar() {
-    "use speedyjs";
-
-    const six = 6;
-
-    !!six || !!3 || false;
-
-    10 || 3 || 1;
-}
