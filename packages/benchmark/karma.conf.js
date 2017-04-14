9edd3162e0118201e7b511f5f9b4d58582e49aee
Micha Reiser <micha@famreiser.ch>
2017-04-14 10:33:23 +0200

    Use chrome stable for tests

--- packages/benchmark/karma.conf.js
+++ packages/benchmark/karma.conf.js
@@ -63,7 +63,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ["Firefox", "ChromeCanary"],
+        browsers: ["Firefox", "Chrome"],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
