d5a685dcb1418feea3c01fda42989cb370b13f23
Micha Reiser <micha@famreiser.ch>
2017-05-15 10:46:41 +0200

    Fix arrayReverse benchmark

--- packages/benchmark/cases/arrayReverse-spdy.ts
+++ packages/benchmark/cases/arrayReverse-spdy.ts
@@ -16,7 +16,7 @@ function swap(array: number[], i1: int, i2: int) {
     "use speedyjs";
 
     const tmp = array[i1];
-    array[i1] = array[i1];
+    array[i1] = array[i2];
     array[i2] = tmp;
 }
 
