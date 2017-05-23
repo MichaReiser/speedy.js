7e824736c7c7c79f011435d2cf437ab49da0be6a
Micha Reiser <micha@famreiser.ch>
2017-05-23 08:29:58 +0200

    Rename test cases to match file name convention

--- packages/benchmark/cases/object-passing.ts
+++ packages/benchmark/cases/object-passing.ts
@@ -1,3 +1,25 @@
-/**
- * Created by micha on 20.05.17.
- */
+class Point {
+    constructor(private x: number, private y: number) {
+
+    }
+
+    distanceTo(other: Point) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+}
+
+
+async function distance(a: Point, b: Point) {
+    "use speedyjs";
+    return a.distanceTo(b);
+}
+
+async function main2() {
+    const center = new Point(0.0, 0.0);
+    const p = new Point(3.4, 3.123);
+
+    return distance(center, p);
+}
+
+
+
